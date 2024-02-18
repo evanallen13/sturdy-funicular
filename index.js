7de9089fbb016fc605c76de0ae4f3c4e57fc48d0
Evan Allen <eallen@xpirit.com>
2024-02-18 11:08:24 -0500

    add home

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ const { MessagingResponse } = require('twilio').twiml;
 
 const app = express();
 
+app.get('/', (req, res) => {
+    console.log("Hello")
+    res.send('Hello World!');
+});
+
 app.post('/sms', (req, res) => {
   const twiml = new MessagingResponse();
+  console.log("Hello")
 
   twiml.message('The Robots are coming! Head for the hills!');
 
   res.type('text/xml').send(twiml.toString());
 });
 
-app.listen(3000, () => {
-  console.log('Express server listening on port 3000');
+app.listen(4000, () => {
+  console.log('Express server listening on port 4000');
 });
